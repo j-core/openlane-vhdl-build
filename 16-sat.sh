20985bb78574a2f7c920a7cb93e42bd5a1d968ec
D. Jeff Dionne <jeff@build180.local>
2023-01-10 16:05:43 +0900

    added z3 sat solver

--- 16-sat.sh
+++ 16-sat.sh
@@ -4,6 +4,7 @@ export PREFIX=/opt/toolflows
 
 cd src
 git clone https://github.com/boolector/boolector
+git clone https://github.com/Z3Prover/z3.git
 
 cd ../build
 
@@ -18,6 +19,14 @@ make -C build -j8
 
 cp build/bin/b* $PREFIX/bin/
 cp deps/btor2tools/bin/btorsim $PREFIX/bin/
+
+mkdir ../z3
+cd ../z3
+
+cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX=$PREFIX ../../src/z3
+
+make -j8
+make install
 
 cd ../..
 
