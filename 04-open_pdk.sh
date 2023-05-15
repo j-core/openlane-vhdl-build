1a0e9bb5ffef2b0e68839b6b6b94a919b3811898
Rob Landley <rob@landley.net>
2023-05-15 01:40:01 -0500

    Update 04 build, removing broken RPM package management.
    Symbiflow wrapped the commerical fork of Red Hat's old Anaconda RPM
    package management system, and then symbiflow's maintainer made it a subrepo
    of skywater-pdk that gets called check build prerequisites... except it's
    checking rpm on dpkg based system and not finding stuff that's there.
    (And it's checking at build time, not at ./configure time.)
    
    The fix: delete the subrepo. The Makefile -include trying to use it drops
    out if the file isn't there. We already provided the build prerequisites.
    To do this, we need to check out the skywater-pdk repo, modify it, and
    specify the replacement repo when calling the open_pdk build. Note that
    we need to remove it twice: it's a subrepo and the Makefile is trying to
    check it out.

--- 04-open_pdk.sh
+++ 04-open_pdk.sh
@@ -1,26 +1,52 @@
 #!/bin/bash
 
-PREFIX=/opt/toolflows
+: ${PREFIX:=/opt/toolflows}
+export PREFIX PATH="$PATH:$PREFIX"/bin
 
-export PATH=$PATH:$PREFIX/bin
+# I really hope this is no longer needed. TODO: look up fd leak fix commit #
+prlimit -p $$ --nofile=65536:65536
 
 echo clone pdk
 
-git clone https://github.com/RTimothyEdwards/open_pdks.git
+if [ -d skywater-pdk ]
+then
+  echo using existing pdk dir
+elif [ -e skywater-pdk.txz ]
+then
+  tar xvf skywater-pdk.txz || exit 1
+else
+  # Check out skywater-pdk ourselves and delete anaconda entirely.
+  git clone https://github.com/google/skywater-pdk &&
+  cd skywater-pdk &&
+  # Delete submodule
+  git rm --cached third_party/make-env &&
+  sed -i '/make-env/d' .gitmodules &&
+  # Delete redundant attempt to check out submodule
+  sed '/conda.mk:/{N;d}' Makefile &&
+  git submodule init &&
+  git submodule update && cd .. || exit 1
+fi
+
+git clone https://github.com/RTimothyEdwards/open_pdks.git || exit 1
 
 echo build pdk
 
 cd open_pdks
 
-ulimit -Hn 65536
-ulimit  -n 65536
 ./configure --prefix=$PREFIX --with-sky130-variants=A --enable-sky130-pdk
 
-echo "make first time..."
-make
-echo "make 2nd time because https://github.com/RTimothyEdwards/open_pdks/issues/315"
-make
-make install
+# Pass our fixed skywater-pdk into the build, both exported (so make -f child
+# processes get it) and on the command line (so attempts to assign to it from
+# the top level makefile are ignored).
+export SKYWATER_PATH="$PWD/../skywater-pdk"
+
+# call make twice (with no args), then call install
+# because https://github.com/RTimothyEdwards/open_pdks/issues/315
+for i in "" "" install
+do
+  echo "${i:-make} the pdk"
+  make SKYWATER_PATH="$SKYWATER_PATH" $i  # Not quoted so empty string drops out
+done
 
 echo SOURCES timestamp
 touch /opt/toolflows/share/pdk/sky130A/SOURCES
