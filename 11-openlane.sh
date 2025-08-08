220906e66408c5337c45a40e393380eb83fd26d7
J <Jeff@CoreSemi.io>
2025-08-08 05:21:52 +0000

    librelane only requires a command line option in the wrapper.  We're patch free for the first time.

--- 11-openlane.sh
+++ 11-openlane.sh
@@ -70,29 +70,8 @@ pip3 install --break-system-packages .
 
 cd /opt/toolflows/share
 
-echo Cloning librelane and patching defaults
+echo Cloning librelane
 git clone https://github.com/librelane/librelane.git
-
-(cd librelane; patch -p1) << 'EOF'
-diff --git a/librelane/flows/cli.py b/librelane/flows/cli.py
-index ebd0e24..8661ced 100644
---- a/librelane/flows/cli.py
-+++ b/librelane/flows/cli.py
-@@ -131,11 +131,11 @@ def cloup_flow_opts(
-     run_options: bool = True,
-     sequential_flow_controls: bool = True,
-     sequential_flow_reproducible: bool = False,
--    pdk_options: bool = True,
-+    pdk_options: bool = "--manual-pdk",
-     log_level: bool = True,
-     jobs: bool = True,
-     accept_config_files: bool = True,
--    volare_by_default: bool = True,
-+    volare_by_default: bool = False,
-     volare_pdk_override: Optional[str] = None,
-     _enable_debug_flags: bool = False,
-     enable_overwrite_flag: bool = False,
-EOF
 
 cd ../../bin
 
@@ -100,7 +79,7 @@ echo Create script to make librelane available as a tool
 cat >> librelane << 'EOF'
 #/bin/bash
 
-PYTHONPATH=$PYTHONPATH:/opt/toolflows/share/librelane python3 -m librelane "$@"
+PYTHONPATH=$PYTHONPATH:/opt/toolflows/share/librelane python3 -m librelane --manual-pdk "$@"
 EOF
 
 chmod +x librelane
