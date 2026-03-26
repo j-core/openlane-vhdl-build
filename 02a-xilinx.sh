3a066c8a916da45273393f79a55bd8e61089bed1
J <Jeff@CoreSemi.io>
2026-03-26 03:56:00 +0000

    Add Xilinx Series 7 FPGA flow

--- 02a-xilinx.sh
+++ 02a-xilinx.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-export PREFIX=/opt/toolflows
+export PREFIX=/home/jeff/opt/toolflows
 
-mkdir src
-cd src
+SRC = `pwd`/src
+BLD = `pwd`/build
+
+mkdir $SRC
+cd $SRC
 
 echo clone Xilinx NextPNR branch
 
@@ -49,3 +52,64 @@ LDFLAGS="-L/opt/homebrew/opt/zstd/lib -L/opt/homebrew/opt/icu4c/lib" cmake ../..
 
 make -j6
 make install
+
+cd "$SRC"/nextpnr-xilinx/xilinx
+
+GM=700
+G=$GM
+
+for i in external/prjxray-db/[aks]*7/xc*-*
+do
+  OUT="$BLD"/xray/$(basename $(dirname $i))
+  PART=$(basename $i)
+  echo "$PART"
+  mkdir -p "$OUT"
+  { pypy3 python/bbaexport.py --device $PART --bba $OUT/$PART.bba &&
+    $BLD/nextpnr-xilinx/bbasm -l $OUT/$PART.bba $OUT/$PART.bin &&
+    rm -f $OUT/$PART.bba || exit 1; } &
+#  sleep 1 &
+
+  PG=`echo "$PART" | sed -e "s/xc7.//" -e "s/[^0-9].*//"`
+  G=`expr $G - $PG`
+#  echo kgates remaining $G
+  [ $G -lt 0 ] && { wait; G=$GM; }
+done
+wait
+
+echo clone db into share
+
+mkdir -p $PREFIX/share
+(cd $PREFIX/share; git clone https://github.com/openxc7/prjxray-db)
+
+cd "$BLD"/xray
+
+for i in `find . -name xc\*.bin`
+do 
+  bn=`basename $i | sed -e s/.bin//`
+  dn=`dirname $i`
+
+  cp -v $i $PREFIX/share/prjxray-db/$dn/$bn/$bn.bin
+done
+
+cd "$SRC"
+
+echo clone project xray
+
+git clone --recursive https://github.com/f4pga/prjxray.git
+
+cd ../build
+mkdir prjxray
+cd prjxray
+
+cmake ../../src/prjxray -DCMAKE_INSTALL_PREFIX=$PREFIX
+
+make -C tools xc7frames2bit
+make -C tools install xc7frames2bit
+
+echo Manually install the python utilities
+
+cd ../../src/prjxray/utils
+mkdir -p $PREFIX/share/prjxray/utils
+
+cp `find . -name \*.py` /home/jeff/opt/toolflows/share/prjxray/utils
+ln -s $PREFIX/share/prjxray/utils/fasm2frames.py $PREFIX/bin/fasm2frames
